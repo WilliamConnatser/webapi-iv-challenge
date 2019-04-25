b99f51de72d0b9d3898ab4818381a34a3b275d36
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-25 18:24:07 -0500

    fixed pipe

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 //require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, _ => console.log(`\n\n \u{1F680} \u{1F680} \u{1F680} \u{1F680} Server running on port ${port} \n\n`))
