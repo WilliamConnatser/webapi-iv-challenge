803d1f4d71a7785f77054f63a2288dc3ea235dd8
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-25 17:59:38 -0500

    added dotenv

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// code away!
+//Todo: Comment out dotenv in production
+require('dotenv').config();
 const server = require('./server');
 
-server.listen(5000, _ => console.log(`\n\n \u{1F680} \u{1F680} \u{1F680} \u{1F680} Server running on port 5000 \n\n`))
+const port = process.env.PORT | 5000;
+
+server.listen(port, _ => console.log(`\n\n \u{1F680} \u{1F680} \u{1F680} \u{1F680} Server running on port ${port} \n\n`))
